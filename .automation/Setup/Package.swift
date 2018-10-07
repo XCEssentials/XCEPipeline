fe79eeb48c15d9bb36865135989928088bc39736
Maxim Khatskevich <maxim@khatskevi.ch>
2018-10-08 02:23:16 +0500

    [REFACTOR] Started to use ‘pod gen’ and ‘ice’ to generate Xcode project
    isntead of ‘Struct’.

--- .automation/Setup/Package.swift
+++ .automation/Setup/Package.swift
@@ -1,5 +1,4 @@
 // swift-tools-version:4.2
-// https://github.com/apple/swift-package-manager/blob/master/Documentation
 // Managed by ice
 
 import PackageDescription
@@ -10,10 +9,10 @@ let package = Package(
         .package(url: "https://github.com/JohnSundell/Files", from: "2.2.1"),
         .package(url: "https://github.com/JohnSundell/ShellOut", from: "2.1.0"),
         .package(url: "https://github.com/nvzqz/FileKit", from: "5.2.0"),
-        .package(url: "https://github.com/XCEssentials/RepoConfigurator", from: "1.8.6"),
+        .package(url: "https://github.com/XCEssentials/RepoConfigurator", from: "1.9.4"),
     ],
     targets: [
         .target(name: "Setup", dependencies: ["ShellOut", "XCERepoConfigurator", "FileKit", "Files"], path: "Sources"),
     ],
-    swiftLanguageVersions: [.v4, .v4_2]
+    swiftLanguageVersions: [.v4_2]
 )
