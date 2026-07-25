0562f999ad35e2499a2bb2aea1bd1c2265a2d07e
mxkh72 <main@mxkh72.dev>
2026-07-26 01:24:12 +0200

    Make Combine async bridge Swift 6 safe

--- Sources/Core/Extensions/Publisher+Helpers.swift
+++ Sources/Core/Extensions/Publisher+Helpers.swift
@@ -29,7 +29,7 @@ extension Publisher
     ///
     /// Credits:
     /// https://medium.com/geekculture/from-combine-to-async-await-c08bf1d15b77
-    func waitForFirstResult() async throws -> Output
+    func waitForFirstResult() async throws -> Output where Output: Sendable
     {
         try await withCheckedThrowingContinuation { continuation in
 
